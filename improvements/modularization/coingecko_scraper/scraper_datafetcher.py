fa905801415c7a6db1328687525dc4b5795fb2cc
AleInc996 <71233145+AleInc996@users.noreply.github.com>
2025-05-27 23:46:48 +0200

    Update scraper_datafetcher.py
    made some progress in the creation of the module related to the market data retrieval

--- improvements/modularization/coingecko_scraper/scraper_datafetcher.py
+++ improvements/modularization/coingecko_scraper/scraper_datafetcher.py
@@ -11,3 +11,31 @@ Module for handling the download of cryptos market data from CoinGecko website.
 TODO: describe what it does (it would ideally work closely with the client module)
 
 """
+
+# importing necessary libraries and modules
+# modules
+from scraper_config import API_KEY, vs_currency, per_page
+from scraper_logger import get_log
+from scraper_client import coingecko_client
+
+# libraries
+import time
+from typing import Any, Dict, Optional # let's explore later the possibility of assigning a type of outcome to each function, already have in mind what to do, let's see first how it comes out
+
+
+LOGGER = get_log(__name__) # logging
+
+def scraper_fetcher(): # defining a function for retrieving market data for cryptos
+    
+    """
+    TODO: describe what it does.
+    """
+    
+    data = [] # initializing storage memory for the market data that will be retrieved
+    gecko_client = coingecko_client() # calling the module which handles the HTTP request
+    page = 1 # starting from the first page of the results obtained through the API
+    
+    while True: # until there is data to retrieve, consider the following
+        # logger
+        
+        # define parameters
