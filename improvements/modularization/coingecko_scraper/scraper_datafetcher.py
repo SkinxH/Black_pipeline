6ba28c31ec659433926e46d6a05cd10e19f4e8f8
AleInc996 <71233145+AleInc996@users.noreply.github.com>
2025-05-28 14:39:13 +0200

    Update scraper_datafetcher.py
    first draft of the market data retriever module is complete

--- improvements/modularization/coingecko_scraper/scraper_datafetcher.py
+++ improvements/modularization/coingecko_scraper/scraper_datafetcher.py
@@ -28,7 +28,12 @@ LOGGER = get_log(__name__) # logging
 def scraper_fetcher(): # defining a function for retrieving market data for cryptos
     
     """
-    TODO: describe what it does.
+    This function retrieves market data for all cryptos by looking at all pages available at CoinGecko website.
+    The parameters of this function, which may be adjustable if we deem it necessary, are:
+        - currency of the market data we are retrieving
+        - the order by which the market cap of cryptos need to be displayed
+        - number of pages to be retrieved from CoinGecko website
+        - number of the starting page
     """
     
     data = [] # initializing storage memory for the market data that will be retrieved
@@ -36,6 +41,39 @@ def scraper_fetcher(): # defining a function for retrieving market data for cryp
     page = 1 # starting from the first page of the results obtained through the API
     
     while True: # until there is data to retrieve, consider the following
-        # logger
+        LOGGER.info("Retrieving page %d", page) # logger information on what page we are working on
         
-        # define parameters
+        parameters = { # these are the parameters which are part of the HTTP request execution function and which are defined in the config module
+            "vs_currency": vs_currency, # data currency
+            "order": order, # TO UNLOCK THE VARIABLE IN THE CONFIG MODULE, it's descending order by market cap
+            "per_page": per_page, # how many cryptos per page to retrieve with the request
+            "page": page} # number of the page we are at
+        
+        try:
+            data = gecko_client.get("/coins/markets", parameters) # trying to get cryptos market data with parameters specified above connecting to coingecko API, the endpoint path is the one provided in the original script
+        except Exception as error:
+            LOGGER.exception("Issue retrieving data at page %d: %s", page, error) # if there is an error, the traceback of what happened is registered in the log file and the while loop is exited
+            break
+        
+        if not data: # if no more data is available
+            LOGGER.info("End of available data is reached at page number %d", page) # logging information
+            break # while loop is exited
+        
+        data.extend(data) # extending the initially empty storing list by appending retrieved data, it should be coins for an amount of 250 pages
+        
+        if len(data) < per_page: # if the length of the found data available is less than 250, it means the current page of the while loop is the last one we are going to get
+            break # therefore we need to exit the while loop here as well
+        
+        page += 1 # dynamically moving to the next page
+        time.sleep(1) # delaying the API connections by 1 second, just to make sure it doesn't get mixed up
+        
+    LOGGER.info("Total number of cryptos retrieved: %d", len(data)) # once the while loop is exited, in whichever case, it might be useful to understand how many cryptos we were able to retrieve market data for
+    return data
+
+
+
+
+
+
+
+
